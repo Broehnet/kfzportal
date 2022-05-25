e835fdc0f81f8cfb62b51bbd07227536a5fe60fa
Broehnet <broehnet@gmail.com>
2022-05-25 13:04:51 +0200

    Neu

---
+++ src/com/company/Constants.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Constants {
+
+    final double SUPER = 2.10;
+    final double SUPERE10 = 2.05;
+    final double DIESEL = 2.00;
+}
--- src/com/company/Kosten.java
+++ src/com/company/Kosten.java
@@ -1,4 +1,53 @@
 package com.company;
 
 public class Kosten {
+
+    private final int currentJahr = Constants.JAHR;
+    private Auto auto;
+    private int jahre;
+    private Queue einzelkosten;
+    private Double gesamtkosten;
+    private int kmProJahr;
+
+    Kosten(Auto auto, int zeitraum) {
+        this.auto = auto;
+        this.jahre = zeitraum - currentJahr;
+    }
+
+    public Queue getEinzelkosten() {
+        return einzelkosten;
+    }
+
+    public Double getGesamtkosten() {
+        return gesamtkosten;
+    }
+
+    public double sprit() {
+        return kmProJahr * (auto.getVerbrauch() / 100) * jahre;
+    }
+
+    public double verbrauchKosten() {
+        return sprit() * Constants.getPrice(auto.getKraftstoff());
+    }
+
+    public double steuern() {
+
+    }
+
+    public double versicherung() {
+
+    }
+
+    public double verschleiss() {
+
+    }
+
+    public double tuev() {
+
+    }
+
+    public void gesamt() {
+
+    }
+
 }
